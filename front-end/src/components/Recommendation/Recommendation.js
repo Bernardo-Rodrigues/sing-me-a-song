2d920ea355f7af9fd08a084b1b4a41b5969b0bee
Bernardo Rodrigues <bernardo.rodrigues06@unifei.edu.br>
2022-05-05 10:51:49 -0300

    test: add e2e test for s for home, top and random pages

--- front-end/src/components/Recommendation/Recommendation.js
+++ front-end/src/components/Recommendation/Recommendation.js
@@ -39,9 +39,9 @@ export default function Recommendation({ name, youtubeLink, score, id, onUpvote 
       <Row>{name}</Row>
       <ReactPlayer url={youtubeLink} width="100%" height="100%" />
       <Row>
-        <GoArrowUp size="24px" onClick={handleUpvote} />
+        <GoArrowUp id={`${name}-upvote`} size="24px" onClick={handleUpvote} />
         {score}
-        <GoArrowDown size="24px" onClick={handleDownvote} />
+        <GoArrowDown id={`${name}-downvote`} size="24px" onClick={handleDownvote} />
       </Row>
     </Container>
   );
